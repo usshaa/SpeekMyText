d4930c63a9eedfdfb2eb12409ac5fadf72044fe1
usshaa <usshaa47@gmail.com>
2024-09-22 09:40:50 +0530

    first commit

--- tts_model.py
+++
@@ -1,8 +0,0 @@
-from TTS.api import TTS
-
-# Initialize the TTS model
-tts = TTS(model_name="tts_models/en/ek1/tacotron2")
-
-def generate_speech(text, voice_sample_path, output_audio_path):
-    # Generate speech using the TTS model
-    tts.tts_to_file(text, speaker_wav=voice_sample_path, file_path=output_audio_path)
